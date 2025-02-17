f34e45e18cc63a9be50e16b67cf0386b499deb66
Klaus-gudy <klausongudy@gmail.com>
2025-02-17 23:29:29 +0300

    Added course and user API endpoints

--- VSA/urls.py
+++ VSA/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/payment/', include('payment.urls')),
+    path('api/courses/', include('course.urls')),
+    path('api/user/', include('user.urls')),
 ]
